707ba048c96ffdda8098f8e1f65e472aec4d1576
Ruslan Rogozhkin <57987419+class-RuslanR@users.noreply.github.com>
2020-03-25 00:49:26 +0200

    Добавьте файлы проекта.

---
+++ ex3_arrayUnique_funcArray_delDublicate/ex3_arrayUnique_funcArray_delDublicate.cpp
@@ -0,0 +1,59 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+void funcEnter(int *arr, int s)
+{
+	for (int i = 0; i < s; ++i)
+	{
+		arr[i] = rand() % s;
+	}
+}
+
+void funcShow(int *arr, int s)
+{
+	for (int i = 0; i < s; ++i)
+	{
+		cout << arr[i] << "   ";
+	}
+	cout << "\n";
+}
+int arrayUnique(int array[], int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		for (int count = 1; count < size; ++count)
+		{
+			if (array[i] == array[i + count])
+			{
+				for (int j = i + count; j < size; ++j)
+				{
+					array[j] = array[j + 1];
+				}
+				--size;
+			}
+		}
+	}
+	return size;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(NULL));
+	int size;
+	cout << "Введите размерность массива: ";
+	cin >> size;
+	int *array = new int[size];
+	funcEnter(array, size);
+	cout << "Массив до удаления элементов-дубликатов:" << endl;
+	funcShow(array, size);
+	int newSize = arrayUnique(array, size);
+	cout << "Новый размер массива: " << newSize << endl;
+	delete[] array;
+	system("pause");
+	return 0;
+}
